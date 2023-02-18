df1a13cddf8e4cbdd9a48b2543dd131824ea582b
yeehll02 <116982357+yeehll02@users.noreply.github.com>
2023-02-18 16:08:09 -0500

    taller 2 python

--- main.py
+++ main.py
@@ -10,21 +10,21 @@ class Asiento:
         self.precio = precio
         self.registro = registro
         
-        def cambiarColor(self, color):
-            if ((color=="rojo") or (color=="verde") or (color=="amarillo") or (color=="negro") or (color=="blanco")):
-                self.color=color
+    def cambiarColor(self, color):
+        if (color=="rojo" or color=="verde" or color=="amarillo" or color=="negro" or color=="blanco"):
+            self.color=color
 class Motor:
     def __init__(self, numeroCilindros, tipo, registro):
         self.numeroCilindros = numeroCilindros
         self.tipo = tipo
         self.registro = registro
         
-        def cambiarRegistro(self, registro):
-            self.registro = registro
+    def cambiarRegistro(self, registro):
+        self.registro = registro
         
-        def asignarTipo(self, tipo):
-            if (tipo=="electrico" or tipo=="gasolina"):
-                self.tipo = tipo
+    def asignarTipo(self, tipo):
+        if (tipo=="electrico" or tipo=="gasolina"):
+            self.tipo = tipo
 class Auto:
     cantidadCreados=0
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
@@ -35,21 +35,22 @@ class Auto:
         self.motor = motor
         self.registro = registro
         
-        def cantidadAsientos(self):
-            numeroAsientos=0
-            for asiento in self.asientos:
-                if (type(asiento) == Asiento):
-                    numeroAsientos+=1
-            return numeroAsientos
+    def cantidadAsientos(self):
+        numeroAsientos=0
+        for asiento in self.asientos:
+            if (type(asiento) == Asiento):
+                numeroAsientos+=1
+        return numeroAsientos
         
-        def verificarIntegridad(self):
-            if (self.registro==self.motor.registro):
-                for asiento in self.asientos:
-                    if (type(asiento)== Asiento and asiento.registro != self.registro):
-                        return "Las piezas no son originales"
-                return "Auto original"   
-            else:
-                 return "Las piezas no son originales"
+    def verificarIntegridad(self):
+        if (self.registro==self.motor.registro):
+            for asiento in self.asientos:
+                if (type(asiento)== Asiento):
+                    if asiento.registro != self.registro:
+                         return "Las piezas no son originales"
+            return "Auto original"   
+        else:
+            return "Las piezas no son originales"
                 
             
         
